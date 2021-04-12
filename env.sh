44ab36becdaeeb6914e014b94c3ddb1a644c88b9
ChillerDragon <ChillerDragon@gmail.com>
2021-04-12 15:41:23 +0200

    Add basic env vars and crools functions

---
+++ env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
+export FDDR_ACC_PATH="$HOME/Desktop/git/FDDraceData/accounts"
+
+export PATH="$PATH:$HOME/Desktop/git-extern/um/bin"
+export PATH="$PATH:$HOME/Desktop/git/lib-teeworlds"
+export PATH="$PATH:$HOME/Desktop/git/twmap/target/release"
+export PATH="$PATH:$HOME/Desktop/git/bam"
+export PATH="$PATH:$HOME/Desktop/git/chillerbot-ux/build"
+
+export PATH="$PATH:$HOME/.luarocks/bin"
+export PATH="$PATH:$HOME/.local/bin"
+
