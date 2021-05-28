f23b093865c1de8e35cf35c5df7a242b62009faf
ChillerDragon <ChillerDragon@gmail.com>
2021-05-28 12:22:02 +0200

    update env vars

--- env.sh
+++ env.sh
@@ -3,13 +3,17 @@
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
-export FDDR_ACC_PATH="$HOME/Desktop/git/FDDraceData/accounts"
+export FDDR_ACC_PATH="$HOME/Desktop/git/fddrace/data/accounts"
 
-export PATH="$PATH:$HOME/Desktop/git-extern/um/bin"
-export PATH="$PATH:$HOME/Desktop/git/lib-teeworlds"
-export PATH="$PATH:$HOME/Desktop/git/twmap/target/release"
 export PATH="$PATH:$HOME/Desktop/git/bam"
 export PATH="$PATH:$HOME/Desktop/git/chillerbot-ux/build"
+export PATH="$PATH:$HOME/Desktop/git/chillerbot-zx/build"
+export PATH="$PATH:$HOME/Desktop/git/ddnet/build"
+export PATH="$PATH:$HOME/Desktop/git-extern/um/bin"
+export PATH="$PATH:$HOME/Desktop/git/lib-teeworlds"
+export PATH="$PATH:$HOME/Desktop/git/libtw2/target/release"
+export PATH="$PATH:$HOME/Desktop/git/server/lib/var/bin"
+export PATH="$PATH:$HOME/Desktop/git/twmap/target/release"
 
 export PATH="$PATH:$HOME/.luarocks/bin"
 export PATH="$PATH:$HOME/.local/bin"
